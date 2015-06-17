f691d45717a866d68a9008004822c27c275cfa68
tylerm <tylermcginnis@gmail.com>
2015-06-17 17:09:55 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'My Project',
-    'author': 'My Name',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'My email.',
+    'description': 'Simple version of grep',
+    'author': 'Tyler McGinnis',
+    'url': 'http://www.github.com/tylerm-/logfind',
+    'author_email': 'tylermcginnis@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'packages': ['logfind'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'logfind'
 }
 
 setup(**config)
