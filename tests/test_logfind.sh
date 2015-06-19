334c056205bb9e99d355b7eb046535ad3c1ff376
tylerm <tylermcginnis@gmail.com>
2015-06-19 15:04:36 +0000

    Added small tests to test_logfind.sh

--- tests/test_logfind.sh
+++ tests/test_logfind.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-python bin/logfind zedshaw
-python bin/logfind "zed has blue eyes"
-python bin/logfind zedshaw -o
-python bin/logfind "zed has blue eyes" -o
+expected="README.md"
+result=$(python bin/logfind "To Do List")
+
+[ $result = $expected ] && echo "Pass" || echo "Fail"
+
+result=$(python bin/logfind "To Do List" -o)
+
+[ $result = $expected ] && echo "Pass" || echo "Fail"
